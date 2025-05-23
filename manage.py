fed9955a841b130c591d923e057671f1b67f9363
Yashini P <142714954+Yashini13@users.noreply.github.com>
2025-05-23 06:36:54 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Django's command-line utility for administrative tasks for the project"""
 import os
 import sys
 
